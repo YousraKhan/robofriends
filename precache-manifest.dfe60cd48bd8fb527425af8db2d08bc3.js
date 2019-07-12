6223f1d7f76ffae32f71b93505385726f77edd2d
YousraKhan <yousra.khan97@hotmail.com>
2019-07-13 02:21:18 +0500

    Updates

---
+++ precache-manifest.dfe60cd48bd8fb527425af8db2d08bc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c343a2bb48203e84f115ea17a9f1b5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "47a5ab1d5f9c9532648a",
+    "url": "/robofriends/static/css/main.ecd07365.chunk.css"
+  },
+  {
+    "revision": "165c83f85a4584eb65d7",
+    "url": "/robofriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "47a5ab1d5f9c9532648a",
+    "url": "/robofriends/static/js/main.1eec9bfd.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
